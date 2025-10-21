9f54e97764a6449a8608a4c014d727e4fd22d63c
Sara Nashat <sarah77nashat77@gmail.com>
2025-10-21 10:55:04 +0300

    addui

---
+++ SafeReport.API/SafeReport.UI/Program.cs
@@ -0,0 +1,35 @@
+using SafeReport.UI.Components;
+using SafeReport.UI.Interfaces;
+using SafeReport.UI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped<IReportService, ReportService>();
+builder.Services.AddHttpClient<IReportService, ReportService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7196/"); 
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
